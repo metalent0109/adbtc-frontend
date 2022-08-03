d3b1204f5d3ae1e15617a404d6ef932e1fb5ef00
metalentco <metalentco777@gmail.com>
2022-08-03 17:33:22 +0200

    modified

--- src/api/api.tsx
+++ src/api/api.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export default axios.create({
-    baseURL: "http://localhost:5000/api/user/",
+    baseURL: "https://btcads-backend.herokuapp.com/api/user/",
     headers: {
         Accept: "application/json",
         "Content-Type": "application/json",
